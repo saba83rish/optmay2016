98103d1a8e582868916a8e211267f94ce9a8ed22
DayanaSunny <dayanams11@gmail.com>
2016-06-24 18:36:16 -0500

    My second commit - Assignment 2 to 4

---
+++ opt-may-2016/Dayana/com/chapter4/assignment4/Tester.java
@@ -0,0 +1,19 @@
+package com.chapter4.assignment4;
+
+public class Tester extends Employee {
+	private String testPlatform;
+
+	
+
+	public Tester(String firstName, String lastName, int employeeId, String salary,String role, String testPlatform) {
+		super(firstName, lastName, employeeId, salary, role);
+		this.testPlatform = testPlatform;
+	}
+	
+	public void jobDescription()
+	{
+		System.out.println(" Tester tests the code and make sure that the application is defect free. Tester works in "+ this.testPlatform);
+	}
+	
+	
+}
